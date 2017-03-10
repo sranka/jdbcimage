20f2ed2f6adb07520b1586c4f763b4844e4ca554
pavel.zavora <pavel.zavora@pricefx.eu>
2017-03-10 19:23:55 +0100

    repaired dumpFileHeader.sh

--- dumpFileHeader.sh
+++ dumpFileHeader.sh
@@ -7,5 +7,5 @@ OPTS="$OPTS -Dtool_in_file=$tool_in_file"
 OPTS="$OPTS -Dtool_out_file=$tool_out_file"
 OPTS=$OPTS "-Dtool_skip_data=true"
 
-java -classpath target/jdbc-image-tools.jar pz.tool.jdbcimage.main.TableFileDump
+java $OPTS -classpath target/jdbc-image-tools.jar pz.tool.jdbcimage.main.TableFileDump
 
